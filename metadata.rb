dc448887a366f1464cb2f659452ecea7f78119d6
Jason Tolsma <jason.tolsma@turner.com>
2013-05-10 13:19:34 -0400

    Major bump because of overwrite changes

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "darrin@heavywater.ca"
 license          "Apache 2.0"
 description      "Installs/Configures route53"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.3.2"
+version          "1.0.0"
 
 depends "build-essential"
