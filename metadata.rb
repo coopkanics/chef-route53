2695ab4799fc1faba8dba2f26ee1bb3c250f0259
Eric Tang <eric.x.tang@gmail.com>
2014-08-06 17:41:44 -0400

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,6 @@ license          "Apache 2.0"
 description      "Installs/Configures route53"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.0"
+name             "route53"
 
 depends "build-essential"
